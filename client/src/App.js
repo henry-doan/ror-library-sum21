dbfbd9263f486b13eff4a4158296d5d7062d3f91
Henry Doan <henrydoan96@gmail.com>
2021-06-23 20:56:53 -0600

    auth done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './components/shared/Home';
+import Nomatch from './components/shared/Nomatch';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Checkouts from './components/checkouts/Checkouts';
+import MainNavbar from './components/shared/MainNavbar';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <MainNavbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <ProtectedRoute exact path="/checkouts" component={Checkouts} />
+          <Route component={Nomatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
+  </>
+)
 
 export default App;
---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,9 @@
+import { Jumbotron } from 'react-bootstrap';
+
+const Home = () => (
+  <Jumbotron>
+    <h1>Home Page</h1>
+  </Jumbotron>
+)
+
+export default Home;
