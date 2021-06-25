bde04fda8fcd3e2cc53532e453c86bff35eeeea8
Henry Doan <henrydoan96@gmail.com>
2021-06-24 19:07:57 -0600

    index and add working

--- client/src/components/checkouts/Checkouts.js
+++ client/src/components/checkouts/Checkouts.js
@@ -1,10 +1,14 @@
 import { Jumbotron } from 'react-bootstrap';
+import CheckoutList from './CheckoutList';
+import CheckoutForm from './CheckoutForm';
 
 const Checkouts = () => (
   <>
     <Jumbotron>
       <h1>Checkouts Page</h1>
     </Jumbotron>
+    <CheckoutForm />
+    <CheckoutList />
   </>
 )
 
